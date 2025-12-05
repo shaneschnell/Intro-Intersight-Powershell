6d5044dc7d1f51cec475ad654efb984dc399ba58
Shane Schnell <shane.schnell@gmail.com>
2025-12-05 07:44:46 -0800

    Add CPU Usage Report

---
+++ Examples/CpuUsageReport/CpuUsageReport.ps1
@@ -0,0 +1,118 @@
+#================================================================================================
+# Report name & output file
+#================================================================================================
+$reportName = "CpuUsageReport"
+$report       = @()
+$date = Get-Date -Format "yyyy-MM-dd_hh-mm"
+
+if ($env:OS -eq "Windows_NT") {
+    $outFile = "$env:USERPROFILE\Downloads\$reportName.$date.xlsx"
+} else {
+    $outFile = "$env:HOME/Downloads/$reportName.$date.xlsx"
+}
+
+#================================================================================================
+# Time window (last 30 days)
+#================================================================================================
+$startTime = [DateTime]::UtcNow.AddDays(-30)
+$endTime   = [DateTime]::UtcNow
+
+$startIso = $startTime.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+$endIso   = $endTime.ToString("yyyy-MM-ddTHH:mm:ss.fffZ")
+
+#================================================================================================
+# Import Modules
+#================================================================================================
+Import-Module Intersight.PowerShell
+Import-Module -Name ImportExcel
+
+#================================================================================================
+# Gather Powered-on hosts that are IMM mode
+#================================================================================================
+$poweredOnHosts = Get-IntersightComputePhysicalSummary | Where-Object { $_.OperPowerState -eq 'on' -and $_.ManagementMode -eq 'Intersight' -and $_.Model -notlike 'UCSB*'} | Select-Object DeviceMoId, Name #-First 10
+
+#================================================================================================
+# Build result objects with per-host CPUAvg
+#================================================================================================
+foreach ($server in $poweredOnHosts) {
+
+  $serverName = $server.Name
+  Write-Host "ServerName: $serverName"
+
+    # Telemetry query JSON
+    $query = @"
+  {
+    "queryType": "groupBy",
+    "dataSource": "PhysicalEntities",
+    "granularity": {
+      "type": "period",
+      "period": "P7D",
+      "timeZone": "America/Los_Angeles",
+      "origin": "$startIso"
+    },
+    "intervals": [
+      "$startIso/$endIso"
+    ],
+    "dimensions": [],
+    "filter": {
+      "type": "and",
+      "fields": [
+        {
+          "type": "selector",
+          "dimension": "host.name",
+          "value": "$serverName"
+        },
+        {
+          "type": "selector",
+          "dimension": "instrument.name",
+          "value": "hw.cpu"
+        }
+      ]
+    },
+    "aggregations": [
+      {
+        "type": "longSum",
+        "name": "count",
+        "fieldName": "hw.cpu.utilization_c0_count"
+      },
+      {
+        "type": "doubleSum",
+        "name": "hw.cpu.utilization_c0-Sum",
+        "fieldName": "hw.cpu.utilization_c0"
+      },
+      {
+        "type": "thetaSketch",
+        "name": "endpoint_count",
+        "fieldName": "host.id"
+      }
+    ],
+    "postAggregations": [
+      {
+        "type": "expression",
+        "name": "hw-cpu-utilization_c0-Avg",
+        "expression": "(\"hw.cpu.utilization_c0-Sum\" / \"count\")"
+      }
+    ]
+  }
+"@
+
+    # Convert JSON string to hashtable for the Intersight call
+    $queryHash = $query | ConvertFrom-Json -AsHashTable
+
+    # Run the query
+    $results = New-IntersightManagedObject -ObjectType telemetry.TimeSerie -AdditionalProperties $queryHash | ConvertFrom-Json
+
+    # Calculate CPU average for THIS server
+    $cpuavg = ($results.event | Measure-Object -Property 'hw-cpu-utilization_c0-Avg' -Average).Average
+
+    $reportRow = New-Object PSObject
+    $reportRow | Add-Member -MemberType NoteProperty -Name "ServerName" -Value $serverName
+    $reportRow | Add-Member -MemberType NoteProperty -Name "DeviceMoId" -Value $server.DeviceMoId
+    $reportRow | Add-Member -MemberType NoteProperty -Name "CPUAvg"     -Value $cpuavg
+    $report += $reportRow
+}
+
+#================================================================================================
+# Export to Excel
+#================================================================================================
+$report | Sort-Object Name | Export-Excel -Path $outFile -WorksheetName 'Overview' -AutoFilter -BoldTopRow
---
+++ Examples/CpuUsageReport/README.md
@@ -0,0 +1,23 @@
+# CPU Usage Report
+This report gathers the Average CPU Usage over the past month for all powered on servers that are using IMM
+
+It will export a xlsx file in the downloads directory.
+
+## Server CPU Metrics
+The Server CPU Metrics (Physical Processor -> Active CPU Utilization) is supported only on X-Series, Rack-Servers M6+ platform only in IMM mode.  The minimum version is 5.2.2.240080
+
+## Requirements
+1. Authenticated to Intersight with API Key.  See instructions [here](/README.md#authentication-to-intersight)
+
+2. Modules:
+   1. ```PowerShell
+      Install-Module -Name Intersight.PowerShell
+      ```
+   1. ```PowerShell
+      Install-Module -Name ImportExcel
+      ```
+
+## Usage
+```Powershell
+.\CpuUsageReport.ps1
+```
